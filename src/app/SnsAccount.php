f8caf8c28998ed4b234a223dd43a1e9d8d509d3c
Shohei Kai <kai.entertainment0304@gmail.com>
2019-11-01 16:02:34 +0900

    add guarded property

--- src/app/SnsAccount.php
+++ src/app/SnsAccount.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SnsAccount extends Model
 {
-    //
+    // user_id は mass assignment で割当可能にしたいので guarded に入れない
+    protected $guarded = [
+        'id', 'created_at', 'updated_at'
+    ];
 }
