b966d39cf55ee2ae3e142cbbf38d69db1f28313b
Shohei Kai <shoheikai@mbp-kai.local>
2019-12-06 14:13:08 +0900

    change route;

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        /** @var App\User $user */
+        $user = new User();
+        $params = [
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['name']),
+        ];
+
+        $user->fill($params)->save();
+
+        return response()->json(['status' => 'success']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(User $user): JsonResponse
+    {
+        $user = User::find($user);
+        return response()->json($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function update(Request $request, User $user): JsonResponse
+    {
+        return response()->json(['status' => 'success']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -11,8 +11,10 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::group(['namespace' => 'Auth'], function () {
-    Route::group(['middle' => 'guest:api'], function () {
+    Route::group(['middleware' => 'guest:api'], function () {
         Route::post('/login', 'AuthController@login');
     });
     Route::group(['middleware' => 'auth:api'], function () {
@@ -20,3 +22,9 @@ Route::group(['namespace' => 'Auth'], function () {
     });
 });
 
+Route::group(['prefix' => 'users'], function () {
+    Route::post('/new', 'UserController@store');
+    Route::get('/', 'UserController@index');
+    Route::get('/{user}', 'UserController@show');
+    Route::put('/{user}/update', 'UserController@update');
+});
